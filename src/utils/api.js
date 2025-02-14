d812a3e0cb79d86c02e691afceaf44ee42e71db2
Gabriel Vidaurre <gabriel.vidaurrerodriguez@windriver.com>
2025-02-14 00:54:42 -0600

    Resolve linter warnings

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-const ALGOLIA_API_KEY = process.env.REACT_APP_ALGOLIA_API_KEY;
-const ALGOLIA_APP_ID = process.env.REACT_APP_ALGOLIA_APP_ID;
-
 export const fetchStores = async (chainId, setStores, setError, PROXY_URL) => {
     try {
         if (chainId === 'chain1') {
